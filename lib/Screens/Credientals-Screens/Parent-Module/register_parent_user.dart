d4781fee5acff65e24d467f922600bc237771815
Ali ijaz <Se.Aliijaz@gmail.com>
2022-11-13 04:46:09 +0500

    register as parent

--- lib/Screens/Credientals-Screens/Parent-Module/register_parent_user.dart
+++ lib/Screens/Credientals-Screens/Parent-Module/register_parent_user.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:women_safety_app/Models/user_model.dart';
 import 'package:women_safety_app/Screens/Credientals-Screens/Child-Module/child_login_screen.dart';
 import 'package:women_safety_app/Widgets/Custom-Widgets/text_form_field.dart';
-
 import '../../../Utils/constants.dart';
 import '../../../Widgets/Custom-Buttons/primary_button.dart';
 import '../../../Widgets/Custom-Buttons/secondary_button.dart';
@@ -21,11 +23,61 @@ class _RegisterParentUserState extends State<RegisterParentUser> {
   bool isLoading = false;
 
   ///onsubmit
-  _onFormSubmit() {
+  _onFormSubmit() async {
     formKey.currentState!.save();
-    customProgressIndicator(context);
-    debugPrint("${formData["email"]}");
-    debugPrint("${formData["password"]}");
+    if (formData['password'] != formData['rpassword']) {
+      showMessage('password and retype password should be equal');
+    } else {
+      customProgressIndicator(context);
+      try {
+        setState(() {
+          isLoading = true;
+        });
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: formData['gemail'].toString(),
+                password: formData['password'].toString());
+        if (userCredential.user != null) {
+          final v = userCredential.user!.uid;
+          DocumentReference<Map<String, dynamic>> db =
+              FirebaseFirestore.instance.collection('users').doc(v);
+
+          final user = UserModel(
+              name: formData['name'].toString(),
+              phone: formData['phone'].toString(),
+              childEmail: formData['cemail'].toString(),
+              guardianEmail: formData['gemail'].toString(),
+              id: v,
+              type: 'parent');
+          final jsonData = user.toJson();
+          await db.set(jsonData).whenComplete(() {
+            goTo(context, ChildLogInScreen());
+            setState(() {
+              isLoading = false;
+            });
+          });
+        }
+      } on FirebaseAuthException catch (e) {
+        setState(() {
+          isLoading = false;
+        });
+        if (e.code == 'weak-password') {
+          debugPrint('The password provided is too weak.');
+          showMessage('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          debugPrint('The account already exists for that email.');
+          showMessage('The account already exists for that email.');
+        }
+      } catch (e) {
+        setState(() {
+          isLoading = false;
+        });
+        debugPrint(e.toString());
+        showMessage(e.toString());
+      }
+    }
+    debugPrint("${formData['email']}");
+    debugPrint("${formData['password']}");
   }
 
   @override
@@ -74,77 +126,75 @@ class _RegisterParentUserState extends State<RegisterParentUser> {
                                 children: [
                                   ///Enter name
                                   CustomTextField(
-                                    labelText: 'Enter Name',
+                                    labelText: 'enter name',
                                     textInputAction: TextInputAction.next,
                                     keyboardType: TextInputType.name,
                                     prefixIcon: Icon(Icons.person),
                                     onSave: (name) {
                                       formData['name'] = name ?? "";
                                     },
-                                    validator: (name) {
-                                      if (name!.isEmpty || name.length < 3) {
-                                        return 'Enter correct Name';
+                                    validator: (email) {
+                                      if (email!.isEmpty || email.length < 3) {
+                                        return 'enter correct name';
                                       }
                                       return null;
                                     },
                                   ),
-
-                                  ///Enter phone
                                   CustomTextField(
-                                    labelText: 'Enter phone',
+                                    labelText: 'enter phone',
                                     textInputAction: TextInputAction.next,
                                     keyboardType: TextInputType.phone,
                                     prefixIcon: Icon(Icons.phone),
                                     onSave: (phone) {
                                       formData['phone'] = phone ?? "";
                                     },
-                                    validator: (phone) {
-                                      if (phone!.isEmpty || phone.length < 10) {
-                                        return 'Enter correct phone';
+                                    validator: (email) {
+                                      if (email!.isEmpty || email.length < 10) {
+                                        return 'enter correct phone';
                                       }
                                       return null;
                                     },
                                   ),
                                   CustomTextField(
-                                    labelText: 'Enter email',
+                                    labelText: 'enter email',
                                     textInputAction: TextInputAction.next,
                                     keyboardType: TextInputType.emailAddress,
-                                    prefixIcon: Icon(Icons.email),
+                                    prefixIcon: Icon(Icons.person),
                                     onSave: (email) {
-                                      formData['cemail'] = email ?? "";
+                                      formData['gemail'] = email ?? "";
                                     },
                                     validator: (email) {
                                       if (email!.isEmpty ||
                                           email.length < 3 ||
                                           !email.contains("@")) {
-                                        return 'Enter correct email';
+                                        return 'enter correct email';
                                       }
                                     },
                                   ),
                                   CustomTextField(
-                                    labelText: 'Enter child email',
+                                    labelText: 'enter child email',
                                     textInputAction: TextInputAction.next,
                                     keyboardType: TextInputType.emailAddress,
-                                    prefixIcon: Icon(Icons.email),
+                                    prefixIcon: Icon(Icons.person),
                                     onSave: (cemail) {
                                       formData['cemail'] = cemail ?? "";
                                     },
-                                    validator: (cemail) {
-                                      if (cemail!.isEmpty ||
-                                          cemail.length < 3 ||
-                                          !cemail.contains("@")) {
-                                        return 'Enter correct email';
+                                    validator: (email) {
+                                      if (email!.isEmpty ||
+                                          email.length < 3 ||
+                                          !email.contains("@")) {
+                                        return 'enter correct email';
                                       }
                                     },
                                   ),
                                   CustomTextField(
-                                    labelText: 'Enter password',
+                                    labelText: 'enter password',
                                     isPassword: isPasswordShown,
                                     prefixIcon: Icon(Icons.vpn_key_rounded),
                                     validator: (password) {
                                       if (password!.isEmpty ||
                                           password.length < 7) {
-                                        return 'Enter correct password';
+                                        return 'enter correct password';
                                       }
                                       return null;
                                     },
@@ -162,13 +212,13 @@ class _RegisterParentUserState extends State<RegisterParentUser> {
                                             : Icon(Icons.visibility)),
                                   ),
                                   CustomTextField(
-                                    labelText: 're-Enter password',
+                                    labelText: 'retype password',
                                     isPassword: isRetypePasswordShown,
                                     prefixIcon: Icon(Icons.vpn_key_rounded),
                                     validator: (password) {
                                       if (password!.isEmpty ||
                                           password.length < 7) {
-                                        return 'Enter correct password';
+                                        return 'enter correct password';
                                       }
                                       return null;
                                     },
